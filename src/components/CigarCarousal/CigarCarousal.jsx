0e1441e8b133e05fc4907848cd8462fb9019c0ea
AcesHigh <cothermanbrannon@gmail.com>
2024-07-15 16:36:26 -0500

    7/15/2024

--- src/components/CigarCarousal/CigarCarousal.jsx
+++ src/components/CigarCarousal/CigarCarousal.jsx
@@ -1,25 +1,25 @@
-import React from "react";
+"use client";
 
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@";
+import React, { useEffect } from "react";
+import styles from "./carousal.module.css";
 
-export function CigarCarousel() {
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
+
+export function EmblaCarousel() {
+  const [emblaRef] = useEmblaCarousel({ loop: false }, [
+    Autoplay({ delay: 5000 }),
+  ]);
+
   return (
-    <Carousel>
-      <CarouselContent>
-        <CarouselItem>...</CarouselItem>
-        <CarouselItem>...</CarouselItem>
-        <CarouselItem>...</CarouselItem>
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <div className={styles.embla} ref={emblaRef}>
+      <div className={styles.embla__container}>
+        <div className={styles.embla__slide}>Slide 1</div>
+        <div className={styles.embla__slide}>Slide 2</div>
+        <div className={styles.embla__slide}>Slide 3</div>
+      </div>
+    </div>
   );
 }
 
-export default CigarCarousel;
+export default EmblaCarousel;
