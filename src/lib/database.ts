9478fc9345e8784eb0c5f3332d3b082133278690
Mark Cotherman <mark@cotherman.com>
2024-08-28 10:09:54 -0500

    Fix

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,36 +1,6 @@
 
 import mongoose from 'mongoose';
 
-interface Connection {
-    isConnected?: number;
-}
-
-const connection: Connection = {}
-
-export const connectDb = async (): Promise<void> => {
-    try {
-        if (connection.isConnected) {
-            console.log('Using existing connection');
-            return;
-        }
-
-        if (!process.env.MONGO) {
-            throw new Error('MongoDB connection string not found');
-        }
-
-        console.log("MongoDB Connection String:", process.env.MONGO);
-
-
-        const db = await mongoose.connect(process.env.MONGO);
-        connection.isConnected = db.connections[0].readyState;
-        console.log('New Mongo Connection!');
-
-    } catch (error) {
-        console.log(error);
-        throw new Error('Unable to connect to database ' + error);
-    }
-};
-
 let initializedTimes: number = 0;
 
 class MainDB {
@@ -46,24 +16,20 @@ class MainDB {
         }
     }
 
-    // private switchClientOrServerConfig() {
-    //   return {
-    //     host: process.env.NEXT_PUBLIC_DATABASE_HOST,
-    //     username: process.env.NEXT_PUBLIC_DATABASE_USERNAME,
-    //     password: process.env.NEXT_PUBLIC_DATABASE_PASSWORD,
-    //   };
-    // }
-
     private async initializeDB() {
         if (!process.env.MONGO) {
             throw new Error('MongoDB connection string not found');
         }
 
-        initializedTimes++;
-        console.log("MongoDB Connection String:", process.env.MONGO);
-        const db = await mongoose.connect(process.env.MONGO);
-        connection.isConnected = db.connections[0].readyState;
-        console.log('New Mongo Connection!');
+        try {
+            initializedTimes++;
+            console.log("MongoDB Connection String:", process.env.MONGO);
+            const db = await mongoose.connect(process.env.MONGO);
+            console.log('New Mongo Connection!');
+        } catch (error) {
+            console.log(error);
+            throw new Error('Unable to connect to database ' + error);
+        }
     }
 
 }
