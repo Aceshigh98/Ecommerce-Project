18379cd9d630a8f53c76c50a75a9192d261c8eec
AcesHigh <cothermanbrannon@gmail.com>
2024-09-03 13:52:21 -0500

    9/3/2024

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ const Home = () => {
         <div className={styles.detailsTextContainer}>
           <h1>GREAT VIBES</h1>
           <p>
-          Smoking a great cigar is more than just a pastime; it's a moment of pure relaxation and indulgence that allows you to savor the finer things in life. As you light up and take that first draw, a sense of calm washes over you, releasing the tension of the day and inviting you to slow down and enjoy the present. The rich, complex flavors of a well-crafted cigar dance across your palate, each puff unfolding new layers of taste that complement a good conversation, a quiet evening, or a reflective moment alone. With every inhale, you're not just smoking a cigar—you're embracing a ritual that enhances your time, turning it into a luxurious experience of peace and enjoyment.
+          Smoking a great cigar is more than just a pastime; it&apos;s a moment of pure relaxation and indulgence that allows you to savor the finer things in life. As you light up and take that first draw, a sense of calm washes over you, releasing the tension of the day and inviting you to slow down and enjoy the present. The rich, complex flavors of a well-crafted cigar dance across your palate, each puff unfolding new layers of taste that complement a good conversation, a quiet evening, or a reflective moment alone. With every inhale, you&apos;re not just smoking a cigar you&apos;re embracing a ritual that enhances your time, turning it into a luxurious experience of peace and enjoyment.
           </p>
         </div>
       </div>
@@ -49,7 +49,7 @@ const Home = () => {
         <div className={styles.cigarOfMonthTextContainer}>
           <h1>View our Inventory</h1>
           <p>
-          Our shop proudly carries a wide variety of cigar brands, wrappers, and sizes to suit every preference, from mild to full-bodied experiences. We feature renowned names such as Cohiba, Montecristo, Arturo Fuente, Padron, and Romeo y Julieta, alongside other top brands celebrated for their quality and craftsmanship. Whether you're looking for a classic favorite or something new to explore, our diverse selection offers something for every cigar enthusiast.
+          Our shop proudly carries a wide variety of cigar brands, wrappers, and sizes to suit every preference, from mild to full-bodied experiences. We feature renowned names such as Cohiba, Montecristo, Arturo Fuente, Padron, and Romeo y Julieta, alongside other top brands celebrated for their quality and craftsmanship. Whether you&apos;re looking for a classic favorite or something new to explore, our diverse selection offers something for every cigar enthusiast.
           </p>
           <Link href={"/shop"}>
             <button className={styles.button}>Shop Now</button>
